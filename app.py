0c84cabce9f2de33513ec0b173db1394ac305fe8
Zuhair Khan <114708014+zkhan21@users.noreply.github.com>
2025-01-28 21:42:07 +0000

    created folders

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-# Sample route
-@app.route('/')
-def home():
-    return "Welcome to SmartExpense Tracker API!"
+from backend.routes import app
 
 if __name__ == '__main__':
     app.run(debug=True)
