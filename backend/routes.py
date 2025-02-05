0c378351e9e579fccbd9fbcedf745bd311d83aba
Ubuntu <ubuntu@ip-172-31-4-182.us-east-2.compute.internal>
2025-02-05 05:07:52 +0000

    Ready for AWS Hosting

--- backend/routes.py
+++ backend/routes.py
@@ -5,7 +5,8 @@ from backend.models import expenses_collection
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Corrected version
 
-app = Flask(__name__, static_folder=os.path.join(BASE_DIR, 'static'), template_folder=os.path.join(BASE_DIR, 'templates'))
+app = Flask(__name__, template_folder=os.path.join(BASE_DIR, '../templates'))
+
 CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
 @app.before_request
